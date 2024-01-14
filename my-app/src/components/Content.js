cf23545d48381ccda887bc532cc6b40bf5492920
Daniel otowski <danielotowski@googlemail.com>
2024-01-14 16:24:15 +0000

    Complete forms challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,7 +8,8 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: []
         }
     }
 
@@ -16,22 +17,36 @@ export class Content extends Component {
         setTimeout(()=>{
             this.setState({
                 isLoaded: true,
+                posts: savedPosts
             })
         }, 2000)
     }
-    
+    handleChange = (event) => {
+        const name = event.target.value.toLowerCase()
+        const filteredPosts = savedPosts.filter(post => {
+            return post.name.toLocaleLowerCase().includes(name)
+        })
+        this.setState({
+            posts: filteredPosts
+        })
+    }
     render() {
         return (
             <div className={css.Content}>
                 
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="searchInput">Search</label>
+                        <input onChange={(event) => this.handleChange(event)} type="search" id="searchInput"/>
+                    </form>
+                    <h4>Posts found: {this.state.posts.length}</h4>
                 </div>
 
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ?
-                        <PostItem savedPosts={savedPosts} />
+                        <PostItem savedPosts={this.state.posts} />
                         : <Loader />
                     }
                 </div>
